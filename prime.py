f6e71ce9659fcfc89ec2361fee5b1fe8b23bba47
godofpainTR <metekaanozden@gmail.com>
2018-09-18 15:22:33 +0300

    release

---
+++ prime.py
@@ -0,0 +1,14 @@
+number = int(input("enter number:"))
+num2 = number
+counter = 0
+primes = []
+for i in range(2,number+1):
+	while number%i == 0:
+		number = number/i
+		counter = counter + 1
+		primes.append(i)
+print(number)
+print(counter," prime factors")
+for a in range(2,num2+1):
+	if a in primes:
+		print("{}^{}".format(primes.count(a),a))
