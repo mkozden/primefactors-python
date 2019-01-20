6b981510d87e58caa5c8247976e30fec2a3bd790
godofpainTR <metekaanozden@gmail.com>
2019-01-20 15:32:09 +0300

    Revert "Process should stop after there are no more primes to be found" because it doesn't work
    This reverts commit 4edfbba0812e3799f3995d254508de8a4f6f67e8.

--- prime.py
+++ prime.py
@@ -2,18 +2,12 @@ number = int(input("enter number:"))
 num2 = number
 counter = 0
 primes = []
-numfinal = 0
 for i in range(2,number+1):
 	while number%i == 0:
 		number = number/i
 		counter = counter + 1
 		primes.append(i)
 print(counter," prime factors")
-while True:
-	for a in range(2,num2+1):
-		if a in primes:
-			print("{}^{}".format(a,primes.count(a)))
-			numfinal += (a**primes.count(a))
-		if num2 == numfinal:
-			break
-	break
+for a in range(2,num2+1):
+	if a in primes:
+		print("{}^{}".format(a,primes.count(a)))
