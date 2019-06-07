e4124b6d041ee8ba502c5dd623eac5f998a0869c
godofpainTR <metekaanozden@gmail.com>
2019-06-07 19:59:18 +0300

    Added remaining time display and reduced print calls to gain back processing time

--- prime.py
+++ prime.py
@@ -1,10 +1,22 @@
 import time
+import sys
 def main():
 	number = int(input("enter number:"))
 	num2 = number
 	counter = 0
 	primesraw = []
 	i = 2
+	def remainingtime(i,number):
+		remaining = round(((time.process_time()*number)/i)-time.process_time(),1)
+		if remaining >= 86400:
+			remaining = str(round(remaining/86400)) + " days left		"
+		elif remaining >= 3600:
+			remaining = str(round(remaining/3600)) + " hours left		"
+		elif remaining >= 60:
+			remaining = str(round(remaining/60)) + " minutes left		"
+		else:
+			remaining = str(remaining) + " seconds left		"
+		return remaining
 	while i <= number:
 		if number%i == 0:
 			number = number/i
@@ -12,12 +24,11 @@ def main():
 			primesraw.append(i)
 		else:
 			i += 1
-		if i < number:
+		if i < number and i%100000 == 0: #limiting the number of print calls to minimize speed loss
 			percentage = round((i / number) * 100, 2)
-		else:
-			percentage = 100
-		print("{}% done!  ({}/{})".format(percentage,i,int(number)), end="\r")
-	print("{}% done in {} seconds.".format(percentage,time.process_time()))
+			print("{}% done! {}".format(percentage,remainingtime(i,number)), end="\r")
+		percentage = 100
+	print("{}% done in {} seconds.								".format(percentage,time.process_time()))
 	print(counter," prime factors")
 	primes = list(dict.fromkeys(primesraw))
 	for a in primes:
