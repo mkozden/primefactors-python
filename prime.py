92310b495d1da9864dbf1f527e932f45a5c192ba
godofpainTR <metekaanozden@gmail.com>
2020-07-30 21:10:42 +0300

    now prints elapsed time while running

--- prime.py
+++ prime.py
@@ -6,7 +6,6 @@ def main(number):
 	counter = 0
 	primesraw = []
 	i = 2
-
 	def elapsedtime():
 		elapsed = round(time.process_time(),2)
 		if elapsed >= 31536000:
@@ -14,23 +13,28 @@ def main(number):
 		elif elapsed >= 86400:
 			elapsed = str(round(elapsed/86400)) + " days 		"
 		elif elapsed >= 3600:
-			elapsed = str(math.floor(elapsed/3600))+" hours "+str(round((elapsed - math.floor(elapsed))*60))+" minutes"
+			elapsed = str(math.floor(elapsed/3600))+" hours "+str(round(math.floor((elapsed - math.floor(elapsed/3600)*3600)/60)))+" minutes"
 		elif elapsed >= 60:
-			elapsed = str(math.floor(elapsed/60))+" minutes "+str(round((elapsed - math.floor(elapsed))*60))+" seconds"
+			elapsed = str(math.floor(elapsed/60))+" minutes "+str(round(elapsed - math.floor(elapsed/60)*60))+" seconds"
 		else:
 			elapsed = str(elapsed) + " seconds 		"
 		return elapsed
+		
 
 	while i <= number**0.5:
 		if number % i == 0:
 			number = int(number//i)
-			counter = counter + 1
+			counter += 1
 			primesraw.append(i)
 		elif i > 2:
 			i += 2
 		else:
 			i += 1
+		percentage = round(i*100/number,2)
+		if i % 50001 == 0:
+			print("{}                           ".format (elapsedtime()), end="\r")
 	primesraw.append(number)
+	counter += 1
 	percentage = 100
 	print("{}% done in {} 								.".format(percentage, elapsedtime()))
 	print(counter, " prime factors")
