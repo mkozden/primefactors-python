90eaed778c065c6acb1b3da7dbb5c9a8d050f2f0
godofpainTR <metekaanozden@gmail.com>
2020-08-11 18:38:00 +0300

    Added a (probably) working progression bar

--- prime.py
+++ prime.py
@@ -6,8 +6,9 @@ def main(number):
 	counter = 0
 	primesraw = []
 	i = 2
+
 	def elapsedtime():
-		elapsed = round(time.process_time(),2)
+		elapsed = round(time.process_time(), 2)
 		if elapsed >= 31536000:
 			elapsed = str(round(elapsed/31536000)) + " years 		"
 		elif elapsed >= 86400:
@@ -19,8 +20,10 @@ def main(number):
 		else:
 			elapsed = str(elapsed) + " seconds 		"
 		return elapsed
-		
 
+	def percgraph(perc):
+		scale = 5
+		return "*"*int(perc//scale)+"-"*int((100//scale)-(perc//scale))
 	while i <= number**0.5:
 		if number % i == 0:
 			number = int(number//i)
@@ -30,13 +33,13 @@ def main(number):
 			i += 2
 		else:
 			i += 1
-		percentage = round(i*100/number,2)
-		if i % 50001 == 0:
-			print("{}                           ".format (elapsedtime()), end="\r")
+		percentage = round(i*100/number**0.5, 2)
+		if i % 5001 == 0:
+			print(f"{percgraph(percentage)} {percentage}% | {elapsedtime()}", end="\r", flush=True)
 	primesraw.append(number)
 	counter += 1
 	percentage = 100
-	print("{}% done in {} 								.".format(percentage, elapsedtime()))
+	print("{}% done in {}.".format(percentage, elapsedtime()), flush=True)
 	print(counter, " prime factors")
 	primes = list(dict.fromkeys(primesraw))
 	for a in primes:
