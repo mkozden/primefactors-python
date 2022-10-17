e92d28e120d0d69c715573a8347aaa0d0aebf613
godofpainTR <metekaanozden@gmail.com>
2022-10-17 19:19:50 +0300

    Optimized main loop, added tqdm progress bar, added expression input

--- prime.py
+++ prime.py
@@ -1,54 +1,52 @@
 import time
-import math
+import os
+from tqdm import tqdm
 
 
 def main(number):
 	counter = 0
 	primesraw = []
 	i = 2
-
-	def elapsedtime(r,x,y):
-		if not r:
-			elapsed = round(time.process_time(), 2)
-		elif r:
-			elapsed = round(time.process_time()*((y**0.5)-x)/x, 2)
-		if elapsed >= 31536000:
-			elapsed = str(round(elapsed/31536000)) + " years 		"
-		elif elapsed >= 86400:
-			elapsed = str(round(elapsed/86400)) + " days 		"
-		elif elapsed >= 3600:
-			elapsed = str(math.floor(elapsed/3600))+" hours "+str(round(math.floor((elapsed - math.floor(elapsed/3600)*3600)/60)))+" minutes"
-		elif elapsed >= 60:
-			elapsed = str(math.floor(elapsed/60))+" minutes "+str(round(elapsed - math.floor(elapsed/60)*60))+" seconds"
-		else:
-			elapsed = str(elapsed) + " seconds 		"
-		return elapsed
-
-	def percgraph(perc):
-		scale = 5
-		return "*"*int(perc//scale)+"-"*int((100//scale)-(perc//scale))
-	while i <= number**0.5:
+	sqrtnum = number ** 0.5
+	pbar = tqdm(total=sqrtnum)
+	while i <= sqrtnum:
 		if number % i == 0:
-			number = int(number//i)
+			number = int(number // i)
+			sqrtnum = number ** 0.5
+			pbar.reset(total=sqrtnum)
+			pbar.update(i)
 			counter += 1
 			primesraw.append(i)
 		elif i > 2:
 			i += 2
+			pbar.update(2)
 		else:
 			i += 1
-		percentage = round(i*100/number**0.5, 2)
-		if i % 50001 == 0:
-			print(f"{percgraph(percentage)} {percentage}% | {elapsedtime(True,i,number)} left", end="\r", flush=True)
+			pbar.update(1)
+	pbar.close()
 	primesraw.append(number)
 	counter += 1
-	percentage = 100
-	print("{}% done in {}.".format(percentage, elapsedtime(False,None,None)), flush=True)
+	os.system("cls||clear")
+	print(f"100% done in {time.process_time()} seconds.")
 	print(counter, " prime factors")
 	primes = list(dict.fromkeys(primesraw))
 	for a in primes:
-			print("{}^{}".format(a, primesraw.count(a)))
+		print("{}^{}".format(a, primesraw.count(a)))
 
 
 if __name__ == "__main__":
-	getnumber = int(input("enter number:"))
-	main(getnumber)
+	getnumber = input("enter number or expression (must result in integer value):")
+	if any(opr in getnumber for opr in ["-", "+", "*", "/", "%"]):
+		try:
+			getnumber = eval(getnumber)
+			if isinstance(getnumber, int):
+				main(getnumber)
+			else:
+				raise TypeError("Not an integer!")
+		except SyntaxError:
+			print("Invalid expression")
+	else:
+		try:
+			main(int(getnumber))
+		except ValueError:
+			print("Not an integer!")
